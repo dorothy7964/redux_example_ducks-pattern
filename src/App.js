8b7b23c0ee127ab533181dba588246c0bedfd55c
SEC\user <dorothy_007@naver.com>
2019-06-14 01:31:12 +0900

    Counter 기본구조

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Palette from './components/Palette';
+import Counter from './components/Counter';
 
 class App extends Component {
   render() {
     return(
       <div className="App">
         <Palette />
+        <Counter />
       </div>
     );
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Counter.css';
+
+const Counter = (props) => {
+  return (
+    <div className="Counter">
+      <h1>value</h1>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  )
+}
+
+export default Counter;
