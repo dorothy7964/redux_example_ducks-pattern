50d3552b4d58c8606be117e6fc983369e8626ba3
SEC\user <dorothy_007@naver.com>
2019-06-16 20:30:37 +0900

    CounterContainer : mapDispatchToProps 수정
    mapDispatchToProps 를 함수형태가 아닌 아예 액션생성함수로 이뤄진 객체를 전달해주면,
    connect 가 발생하게 될 때 bindActionCreators 를 자동으로 해준다.

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'; // **** (1) 불러오기
 import Counter from '../components/Counter';
 import { increment, decrement } from '../store/modules/counter';
 
@@ -29,8 +28,8 @@ const mapStateToProps = ({ counter }) => ({
   number: counter.number,
 });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ increment, decrement }, dispatch); // **** (2) bindActionCreators 사용.
+// **** 함수가 아닌 객체 설정시 자동 bindActionCreators 됨
+const mapDispatchToProps = { increment, decrement };
 
 export default connect(
   mapStateToProps,
