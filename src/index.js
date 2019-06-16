c600453831f01782b87f10edb2e21ac99bb19697
SEC\user <dorothy_007@naver.com>
2019-06-16 20:05:19 +0900

    Store 생성

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// **** (1) createStore 와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
+// **** (3) Provider 불러오기
+import { Provider } from 'react-redux';
+
+// **** (2) 스토어를 만들고 현재 값 확인해보기
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// **** (3) Provider 렌더링해서 기존의 App 감싸주기
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
